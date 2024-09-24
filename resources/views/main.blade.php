47864626e54fcf73174d7f90a9dd4f697e86cd28
guitiorr <officialglude12345@gmail.com>
2024-09-24 22:48:47 +0700

    save

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,10 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome!</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
-
+    @vite(['resources/css/app.css'])
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Handjet:wght@100..900&display=swap');
+    </style>
 </head>
 <body>
-    <h1 class="">Hello Coy</h1>
+
+    <div class="opener">
+        <div class="">
+            <p class="">Hello! Welcome to my <span class="profilespan">PROFILE!</span></p>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+          </svg>
+    </div>
+
+    <div class="about me">
+        <div class="">
+            <p>About ME!</p>
+        </div>
+    </div>
 </body>
 </html>
+
