3d1862de1a50049ccf6cc661f7cd2390f8ecfd52
guitiorr <officialglude12345@gmail.com>
2024-09-26 21:19:23 +0700

    add projects

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,30 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome!</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Handjet:wght@100..900&display=swap');
     </style>
 </head>
+
 <body>
 
     <div class="headerContainer">
         <div class="linksContainer"> <!-- Updated class name -->
             <div class="github">
                 <a class="github" href="https://github.com/guitiorr" target="_blank" rel="noopener noreferrer">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
-                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor"
+                        class="bi bi-github" viewBox="0 0 16 16">
+                        <path
+                            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
                     </svg>
                 </a>
             </div>
             <div class="linkedin">
-                <a class="linkedin" href="https://www.linkedin.com/in/benedictus-cristiano-budi-472a08225/" target="_blank" rel="noopener noreferrer">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+                <a class="linkedin" href="https://www.linkedin.com/in/benedictus-cristiano-budi-472a08225/"
+                    target="_blank" rel="noopener noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor"
+                        class="bi bi-linkedin" viewBox="0 0 16 16">
+                        <path
+                            d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                     </svg>
                 </a>
             </div>
@@ -54,14 +61,136 @@
 
     <div class="aboutContainer">
         <div class="aboutme">
+            <span class="dot">_</span>
             <h1>About me</h1>
         </div>
+        <div class="">
+            <img src="https://m.media-amazon.com/images/S/pv-target-images/dbf6812f59e5080cf420f1056bfceb66f7d6a43a8df19ace503ea70596afc0ff._SX1080_FMjpg_.jpg"
+                alt="">
+        </div>
+    </div>
 
+    <div class="portfolioContainer">
+        <div class="myproject">
+            <h1>My projects</h1><span class="dot">_</span>
+        </div>
+        <div class="projectsContainer">
+            <div class="project1">
+                <div class="projectHeader">
+                    <div class="projectTitle">
+                        <h1>Bot Budi</h1>
+                    </div>
+                    <div class="projectTags">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor"
+                            class="bi bi-discord" viewBox="0 0 16 16">
+                            <path
+                                d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612" />
+                        </svg>
+                        <img width="25" height="25"
+                            src="https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/files/python-logo-only.svg"
+                            alt="">
+                    </div>
+                </div>
+                <div class="projectBody">
+                    Discord Music Bot i made back in senior high school.
+                </div>
+                <img class="pyimg"
+                    src="https://private-user-images.githubusercontent.com/74807734/370537977-6a2437d2-d2f7-42b4-aad0-7af9ab8d7e40.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjczNDUyMzQsIm5iZiI6MTcyNzM0NDkzNCwicGF0aCI6Ii83NDgwNzczNC8zNzA1Mzc5NzctNmEyNDM3ZDItZDJmNy00MmI0LWFhZDAtN2FmOWFiOGQ3ZTQwLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA5MjYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwOTI2VDEwMDIxNFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTJkNDdlYjU0ZTMwMmNhMDAxOWRlYTY5MTI5ZTczZGJkYzRhNWE1MzgwNzc0YzQwMzA4YWQwMGFmN2U3YzhkNGUmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.5wvum1FB4xJV8bO1ovkEd_YmmnK6MBs374aqvDaEVFw"
+                    alt="">
+                <div class="projectLink">
+                    <a href="https://github.com/guitiorr/bot-budi" class="">
+                        View Project
+                    </a>
+                </div>
+            </div>
+            <div class="project1">
+                <div class="projectHeader">
+                    <div class="projectTitle">
+                        <h1>AI Disease Prediction</h1>
+                    </div>
+                    <div class="projectTags">
+                        <img src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/189_Kaggle_logo_logos-512.png" alt="">
+                        <img src="https://seeklogo.com/images/S/streamlit-logo-1A3B208AE4-seeklogo.com.png" alt="">
+                        <img
+                            src="https://s3.dualstack.us-east-2.amazonaws.com/pythondotorg-assets/media/files/python-logo-only.svg"
+                            alt="">
+                    </div>
+                </div>
+                <div class="projectBody">
+                    Final project for 3rd semester Artificial Intelligence course using Python, Streamlit and Kaggle dataset.
+                </div>
+                <img class="pyimg"
+                    src="https://private-user-images.githubusercontent.com/74807734/371107405-732a9e05-e98e-4d87-af9d-cecdb941b1ec.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjczNDg3OTksIm5iZiI6MTcyNzM0ODQ5OSwicGF0aCI6Ii83NDgwNzczNC8zNzExMDc0MDUtNzMyYTllMDUtZTk4ZS00ZDg3LWFmOWQtY2VjZGI5NDFiMWVjLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA5MjYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwOTI2VDExMDEzOVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTY5ZGU2NWRjODFjMzhjM2VjOTkwYzg3NTViYjUxZGFjZWE3OGFkZmNjNDA4ZjVhYWVmZDA1NTI3MmQxOGE2YjEmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.4-BPnxrBV7OmA6mugvxtSEWwYHatsD3VM9Y-HB57o78"
+                    alt="">
+                <div class="projectLink">
+                    <a href="https://github.com/guitiorr/Prototype-AI-Sem-3" class="">
+                        View Project
+                    </a>
+                </div>
+            </div>
+
+            <div class="project1">
+                <div class="projectHeader">
+                    <div class="projectTitle">
+                        <h1>PerfoFit</h1>
+                    </div>
+                    <div class="projectTags">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/.NET_Core_Logo.svg/2048px-.NET_Core_Logo.svg.png" alt="">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Logo_C_sharp.svg/1200px-Logo_C_sharp.svg.png" alt="">
+                    </div>
+                </div>
+                <div class="projectBody">
+                    Fitness website made using ASP.NET framework - 4th semester project. I did most of the backend work.
+                </div>
+                <img class="pyimg"
+                    src="https://private-user-images.githubusercontent.com/74807734/371162572-6dc1ad95-286d-4d0d-a195-d1e04f85f0f1.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjczNTc2MDEsIm5iZiI6MTcyNzM1NzMwMSwicGF0aCI6Ii83NDgwNzczNC8zNzExNjI1NzItNmRjMWFkOTUtMjg2ZC00ZDBkLWExOTUtZDFlMDRmODVmMGYxLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA5MjYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwOTI2VDEzMjgyMVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPTUyMTY4YjFlMjhmMDEzOTMyMjExYTAyNzUwNWJiMTEyYTEyYTllY2IyZmQ3ZGM2ODczNjI3YmYyMDk3YzJjZDImWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.HjCyc7E-fLZN-bU_i4oJSl85WMdrYB9AHPvedRbIEAQ"
+                    alt="">
+                <div class="projectLink">
+                    <a href="https://github.com/guitiorr/FitnessApp" class="">
+                        View Project
+                    </a>
+                </div>
+            </div>
+
+            <div class="project1">
+                <div class="projectHeader">
+                    <div class="projectTitle">
+                        <h1>Second Chance</h1>
+                    </div>
+                    <div class="projectTags">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bootstrap-fill" viewBox="0 0 16 16">
+                            <path d="M6.375 7.125V4.658h1.78c.973 0 1.542.457 1.542 1.237 0 .802-.604 1.23-1.764 1.23zm0 3.762h1.898c1.184 0 1.81-.48 1.81-1.377 0-.885-.65-1.348-1.886-1.348H6.375z"/>
+                            <path d="M4.002 0a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V4a4 4 0 0 0-4-4zm1.06 12V3.545h3.399c1.587 0 2.543.809 2.543 2.11 0 .884-.65 1.675-1.483 1.816v.1c1.143.117 1.904.931 1.904 2.033 0 1.488-1.084 2.396-2.888 2.396z"/>
+                          </svg>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/800px-CSS3_logo.svg.png" alt="">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/HTML5_Badge.svg/2048px-HTML5_Badge.svg.png"
+                            alt="">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpcY0MEsC-KK86cNWulub7GLHSZ9PMWuCqgA&s" alt="">
+                    </div>
+                </div>
+                <div class="projectBody">
+                    My first Hackathon Project! (Garuda Hacks 5.0) i mostly did some frontend work, this event opened my mind to learn more!
+                </div>
+                <img class="pyimg"
+                    src="https://private-user-images.githubusercontent.com/74807734/371166414-51dccbeb-d331-4a72-b519-662903d1bf61.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjczNTgxMzAsIm5iZiI6MTcyNzM1NzgzMCwicGF0aCI6Ii83NDgwNzczNC8zNzExNjY0MTQtNTFkY2NiZWItZDMzMS00YTcyLWI1MTktNjYyOTAzZDFiZjYxLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDA5MjYlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQwOTI2VDEzMzcxMFomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWYxNmE5NzlkYTE1NDg5ZDkxZWQxOGZhMmJhMzI4YTgxMmFiNWM1N2ZiNTliYmQ5OTYyYWMwNTZjMTg1ZmEzNWYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.FTH25Stj9zI2VXasKnnhVjJoYpSxQ-qUYVu6tFL7lco"
+                    alt="">
+                <div class="projectLink">
+                    <a href="https://github.com/jamesalv/GH5" class="">
+                        View Project
+                    </a>
+                </div>
+            </div>
+
+        </div>
 
 
     </div>
 
 
-</body>
-</html>
+        <script></script>
 
+
+</body>
+
+</html>
